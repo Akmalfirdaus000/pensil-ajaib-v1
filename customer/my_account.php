f0ada293491a4f59cecaa3b842ec7745a7ab92d1
citraFebriawirti <febriawirticitra@gmail.com>
2024-10-25 18:13:25 +0700

    Change : style my account

--- customer/my_account.php
+++ customer/my_account.php
@@ -1,21 +1,25 @@
-<?php 
+<?php
 session_start();
 if (!isset($_SESSION['customer_email'])) {
   echo "<script>window.open('../checkout.php','_self')</script>";
-}else{
+} else {
 
-include("../includes/db.php");  
-include("../functions/functions.php");
+  include("../includes/db.php");
+  include("../functions/functions.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pensil Ajaib</title>
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap 5.3 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- owl carousel css file cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
@@ -25,201 +29,188 @@ include("../functions/functions.php");
 
     <!-- custom css file link  -->
     <link rel="stylesheet" href="../style.css">
-  <style>
+    <style>
 
 
-  </style>
- 
+    </style>
+
 </head>
+
 <body>
 
-<!-- header section starts  -->
+    <!-- header section starts  -->
 
-<header>
+    <?php
+    include(".././includes/wa.php");
+    ?>
+    <!-- header section starts  -->
+    <div class="header-1">
 
-<div class="header-1">
+        <!-- <a href="index.php" class="logo" > <img src="website/all/logo5.svg" alt="Logo image" class="hidden-xs">  </a> -->
 
-    <!-- <a href="../index.php" class="logo" > <img src="../website/all/logo5.svg" alt="Logo image" class="hidden-xs">  </a> -->
-                               
-<div class="col-md-6 offer">
-    <a href="#" class="btn btn-sucess btn-sm">
-           <?php
+        <div class="col-md-8 offer">
+            <marquee behavior="" direction="">
+                <h1>SELAMAT DATANG DI PT DUDU DIGITAL INDONESIA</h1>
+            </marquee>
+        </div>
 
-        if (!isset($_SESSION['customer_email'])){
-        echo "Welcome Guest";
-      }else{
-      echo "Welcome: " .$_SESSION['customer_email'] . "";
-    }
+    </div>
+    <!-- navbar section starts -->
+    <header>
+        <div class="header-2">
+            <nav class="navbar navbar-expand-lg navbar-light " style="background: #0ca6ed;">
+                <div class="container-fluid">
+                    <!-- Company Logo on the left -->
+                    <a class="navbar-brand me-3" href="index.php">
+                        <img src=".././website/all/pensilajaib.png" alt="Company Logo" width="70" height="70"
+                            class="d-inline-block align-text-top">
+                    </a>
 
+                    <!-- Toggle button for mobile view -->
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <!-- Collapsible content -->
+                    <div class="collapse navbar-collapse" id="navbarContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="font-size: 15px;">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="../index.php">Beranda</a>
+                            </li>
+                            <!-- <li class="nav-item">
+                                <a class="nav-link" href="#footer">Tentang Kita</a>
+                            </li> -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="../trimer.php">Orderan</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="../contactus.php">Kontak</a>
+                            </li>
+                        </ul>
+                        <!-- Right-aligned items -->
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0" style="font-size: 15px;">
+                            <li class="nav-item">
+                                <a class="nav-link" href="../cart.php">
+                                    <i class="fa fa-shopping-cart"></i>
+                                    <span><?php item(); ?> items in cart</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="../customer_registration.php">
+                                    <i class="fa fa-user-plus"></i> Register
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <?php
+                  if (!isset($_SESSION['customer_email'])) {
+                    echo "<a class='nav-link' href='../checkout.php'>My Account</a>";
+                  } else {
+                    echo "<a class='nav-link' href='customer/my_account.php?my_order'>My Account</a>";
+                  }
+                  ?>
+                            </li>
+
+                            <li class="nav-item">
+                                <?php
+                  if (!isset($_SESSION['customer_email'])) {
+                    echo "<a class='nav-link' href='../checkout.php'>Login</a>";
+                  } else {
+                    echo "<a class='nav-link' href='../logout.php'>Logout</a>";
+                  }
+                  ?>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+
+    <!-- header section End  -->
+
+    <section class="content" id="content" style=" padding: 40px 0;">
+        <div class="container" style="max-width: 800px; margin: 0 auto;">
+            <h4 class="text-center"
+                style="font-size: 28px; color: #343a40; margin-bottom: 20px; font-weight: bold; text-transform: uppercase;">
+                My Account
+            </h4>
+        </div>
+    </section>
+    <div>
+        <div class="col-m-9" style="color: #000;">
+
+
+
+            <?php
+        if (isset($_GET['my_order'])) {
+          include("my_order.php");
+        }
+        ?>
+
+            <!-- including my_order.php End  -->
+
+            <!-- including payoffline.php page starts  -->
+            <?php
+
+        if (isset($_GET['pay_offline'])) {
+          include("pay_offline.php");
+        }
 
         ?>
-    </a>
-<a id="pr" href="#"> Shopping Cart Total Price: INR <?php totalPrice(); ?>, Total Items <?php item(); ?></a>
-</div>
-  
-</div>
 
-<div class="header-2">
-   
+            <!-- including payoffline.php page End  -->
+            <!-- including Edit_account.php page start  -->
+            <?php
+        if (isset($_GET['edit_act'])) {
+          include("edit_act.php");
+        }
+        ?>
 
-<nav class="navbar"> 
-
-
-     <ul >
-       
-            <li><a  href="../index.php">Awal</a></li>
-            <li><a  href="../trimer.php">Orderan</a></li>
-        
-            <li><a href="../contactus.php">Kontak</a></li>
-         
- 
-       <div class="col-md-6">
-        <ul class="menu">
-            <li>
-                         <div class="collapse clearfix" id="search">
-                             <form class="navbar-form" method="get" action="result.php">
-                                 <!-- <div class="input-group">
-                                     <input type="text" name="user_query" placeholder="search" class="form-control" required="">
-                                     <button type="submit" value="search" name="search" class="btn btn-primary">
-                                         <i class="fa fa-search"></i>
-                                     </button>
-                                 </div> -->
-                             </form>
-                         </div>
-                   </li>
-
-
-
-                <li>
-                  <a href="../cart.php" class="">
-                    <i class="fa fa-shopping-cart"></i>
-                      <span><?php item(); ?> items in cart</span>
-                        </a>
-                </li>
-                   
-
-                   <!-- <li>
-                   <a  href="../customer_registration.php"><i class="fa fa-user-plus"></i>Register</a></li> -->
-                   <li>
-                   <?php
-
-                    if (!isset($_SESSION['customer_email'])){
-                    echo "<a href='checkout.php'>My Account</a>";
-
-                         } else{
-                    
-                    echo "<a href='../customer/my_account.php?my_order' class='active'>My Account</a>";
-                
-                         }
-
-                    ?></li> 
-                     
-                   <li>
-                   <a href="../cart.php"><i class="fa fa-shopping-cart"></i>Goto Cart</a></li>
-                    
-                   <li>
-                     <?php
-
-                    if (!isset($_SESSION['customer_email'])){
-                    echo "<a href='checkout.php'>Login</a>";
-
-                         } else{
-                    
-                    echo "<a href='../logout.php'>Logout</a>";
-                
-                         }
-
-                    ?></li>
-             </ul>
-       </div>
-</ul>
-
-
-
-</nav></div></header>
-
-<!-- header section End  -->
-
-<section class="content" id="content">
-  <div class="container">
-    <div class="col-md-12">
-      <ul class="breadcrumb">
-     
-        <li><span>My Account</span></li>
-        
-
-      </ul>
-
-    </div>
-</div></section> 
- <div class="col-m-9">
-
-  <!-- including my_order.php starts  -->
-
-   <?php
-     if (isset($_GET['my_order']))
-     {
-      include("my_order.php");
-     }
-     ?>
-
-     <!-- including my_order.php End  -->
-
-     <!-- including payoffline.php page starts  -->
-<?php
-
-if (isset($_GET['pay_offline'])){
-  include("pay_offline.php");
-}
-
-  ?>
-
-     <!-- including payoffline.php page End  -->
-       <!-- including Edit_account.php page start  -->
-         <?php
-            if (isset($_GET['edit_act'])){
-              include("edit_act.php");
-            }
-           ?>
-
-         <!-- including Edit_account.php page End  -->
-           <!-- including change_pass.php page Start  -->
-         <?php
-            if (isset($_GET['change_pass'])){
-              include("change_password.php");
-            }
-           ?>
+            <!-- including Edit_account.php page End  -->
+            <!-- including change_pass.php page Start  -->
+            <?php
+        if (isset($_GET['change_pass'])) {
+          include("change_password.php");
+        }
+        ?>
 
             <!-- including change_pass.php page End  -->
-             <!-- including delete_pass.php page Start  -->
+            <!-- including delete_pass.php page Start  -->
 
-                <?php
-            if (isset($_GET['delete_ac'])){
-              include("delete_ac.php");
-            }
-           ?>
+            <?php
+        if (isset($_GET['delete_ac'])) {
+          include("delete_ac.php");
+        }
+        ?>
 
-              <!-- including delete_pass.php page End  -->
- </div>
+            <!-- including delete_pass.php page End  -->
+        </div>
 
-    
-  
-  <div class="content1" id="content1">
-  <div class="container1">
-    <div class="col-md-3">
-      <?php
-      include("includes/sidebar.php");  
-      ?>
-   
+
+
+        <div class="content1" id="content1">
+            <div class="container1">
+                <div class="col-md-3">
+                    <?php
+            include("includes/sidebar.php");
+            ?>
+
+                </div>
+
+            </div>
+        </div>
     </div>
 
-</div>
-     </div>
+    <!-- footer section starts  -->
+    <?php
+    include("../includes/footer.php");
+    ?>
+    <!-- footer section   -->
 
-       <!-- footer section starts  -->
-   <?php
-      include("includes/footer.php");  
-      ?>
-<!-- footer section   -->
+    <?php } ?>
+</body>
 
-<?php } ?></body></html>
+</html>
--- customer/my_order.php
+++ customer/my_order.php
@@ -1,64 +1,69 @@
-
 <div class="trx">
 	<center>
-	<h1>My Order</h1>
-	<p>Shipping and additional costs are calculated based on the values you have entered</p>
-    </center>
-    <hr>
-     <div class="tae-responve">
-     	<table class="tab">
-     		<thead>
-     			<tr>
-     				<th>Sr.No</th>
-     				<!-- <th>Due Amount</th> -->
-     				<th>Orderan Number</th>
-     				<!-- <th>Quantity</th> -->
-     				<!-- <th>Size</th> -->
-     				<th>Order Date</th>
-     				<th>complete / Pending</th>
-     				<th>Status</th>
-     			</tr>
-     		</thead>
-     		<tbody>
+		<h1>My Order</h1>
+		<p>Shipping and additional costs are calculated based on the values you have entered</p>
+	</center>
+	<hr>
+	<div class="tae-responve">
+		<table class="tab">
+			<thead>
+				<tr>
+					<th>Sr.No</th>
+					<!-- <th>Due Amount</th> -->
+					<th>Orderan Number</th>
+					<!-- <th>Quantity</th> -->
+					<!-- <th>Size</th> -->
+					<th>Order Date</th>
+					<th>complete / Pending</th>
+					<th>Status</th>
+				</tr>
+			</thead>
+			<tbody>
 
-                    <?php
-                    $customer_session= $_SESSION['customer_email'];
-                    $get_customer="select * from customers where customer_email='$customer_session'";
-                    $run_cust=mysqli_query($con, $get_customer);
-                    $row_cust=mysqli_fetch_array($run_cust);
-                    $customer_id=$row_cust['customer_id'];
-                    $get_order="select * from customer_order where customer_id='$customer_id'";
-                    $run_order=mysqli_query($con, $get_order);
-                    $i=0;
-                    while ($row_order=mysqli_fetch_array($run_order)) {
-                         $order_id=$row_order['order_id'];
-                         $order_due_amount=$row_order['due_amount'];
-                         $order_invoice=$row_order['invoice_no'];
-                         $order_qty=$row_order['qty'];
-                         $order_size=$row_order['size'];
-                         $order_date=substr($row_order['order_date'], 0,11);
-                         $order_status=$row_order['order_status'];
-                         $i++;
-                         if ($order_status=='pending') {
-                              $order_status='Pending';
-                         }else{
-                            $order_status='Complete';  
-                         }
-                   
+				<?php
+				$customer_session = $_SESSION['customer_email'];
+				$get_customer = "SELECT * FROM customers WHERE customer_email='$customer_session'";
+				$run_cust = mysqli_query($con, $get_customer);
+				$row_cust = mysqli_fetch_array($run_cust);
+				$customer_id = $row_cust['customer_id'];
 
-                      ?>
-     			<tr>
-     				<td><?php echo $i ?></td>
-     				<!-- <td><?php echo $order_due_amount ?></td> -->
-     				<td><?php echo $order_invoice ?></td>
-     				<!-- <td><?php echo $order_qty ?></td> -->
-     				<!-- <td><?php echo $order_size ?></td> -->
-     				<td><?php echo $order_date ?></td>
-     				<td><?php echo $order_status ?></td>
-     				<td><a href="confirm.php?order_id=<?php echo $order_id ?>" target="_blank" class="btn btn-primary btn-sm">confirm Now</a></td>
-     			</tr>
-     			<?php } ?>
-     		</tbody>
-     	</table>
-     </div>
+				// Update the SQL query to order by order_date in descending order
+				$get_order = "SELECT * FROM customer_order WHERE customer_id='$customer_id' ORDER BY order_date DESC";
+				$run_order = mysqli_query($con, $get_order);
+				$i = 0;
+
+				while ($row_order = mysqli_fetch_array($run_order)) {
+					$order_id = $row_order['order_id'];
+					$order_due_amount = $row_order['due_amount'];
+					$order_invoice = $row_order['invoice_no'];
+					$order_qty = $row_order['qty'];
+					$order_size = $row_order['size'];
+					$order_date = substr($row_order['order_date'], 0, 11);
+					$order_status = $row_order['order_status'];
+					$i++;
+
+					if ($order_status == 'pending') {
+						$order_status = 'Pending';
+					} else {
+						$order_status = 'Complete';
+					}
+
+					// Add your output code here (e.g., echo or display order details)
+
+				?>
+
+					<tr>
+						<td><?php echo $i ?></td>
+						<!-- <td><?php echo $order_due_amount ?></td> -->
+						<td><?php echo $order_invoice ?></td>
+						<!-- <td><?php echo $order_qty ?></td> -->
+						<!-- <td><?php echo $order_size ?></td> -->
+						<td><?php echo $order_date ?></td>
+						<td><?php echo $order_status ?></td>
+						<td><a href="confirm.php?order_id=<?php echo $order_id ?>" target="_blank" class="btn btn-primary btn-sm">confirm Now</a></td>
+					</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+	</div>
 </div>
